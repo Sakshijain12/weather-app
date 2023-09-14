034bb6253fc7a45ed977c99dcb3f6f6fc16e7b7e
Sakshijain12 <jain.60@iitj.ac.in>
2023-09-14 20:40:24 +0530

    changes done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,6 +11,8 @@ import { StatusBar } from 'expo-status-bar';
 import { weatherImages } from '../constants';
 import { getData, storeData } from '../utils/asyncStorage';
 
+import { LineChart } from 'react-native-chart-kit';
+
 export default function HomeScreen() {
   const [showSearch, toggleSearch] = useState(false);
   const [locations, setLocations] = useState([]);
@@ -18,145 +20,145 @@ export default function HomeScreen() {
   const [weather, setWeather] = useState({})
 
 
-  const handleSearch = search=>{
-    if(search && search.length>2)
-      fetchLocations({cityName: search}).then(data=>{
+  const handleSearch = search => {
+    if (search && search.length > 2)
+      fetchLocations({ cityName: search }).then(data => {
         setLocations(data);
       })
   }
 
-  const handleLocation = loc=>{
+  const handleLocation = loc => {
     setLoading(true);
     toggleSearch(false);
     setLocations([]);
     fetchWeatherForecast({
       cityName: loc.name,
       days: '7'
-    }).then(data=>{
+    }).then(data => {
       setLoading(false);
       setWeather(data);
-      storeData('city',loc.name);
+      storeData('city', loc.name);
     })
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchMyWeatherData();
-  },[]);
+  }, []);
 
-  const fetchMyWeatherData = async ()=>{
+  const fetchMyWeatherData = async () => {
     let myCity = await getData('city');
     let cityName = 'Jodhpur';
-    if(myCity){
+    if (myCity) {
       cityName = myCity;
     }
     fetchWeatherForecast({
       cityName,
       days: '7'
-    }).then(data=>{
+    }).then(data => {
       // console.log('got data: ',data.forecast.forecastday);
       setWeather(data);
       setLoading(false);
     })
-    
+
   }
 
   const handleTextDebounce = useCallback(debounce(handleSearch, 1200), []);
 
-  const {location, current} = weather;
+  const { location, current } = weather;
 
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} >
-    <View className="flex-1 relative">
-      <StatusBar style="light" />
-      <Image 
-        blurRadius={5} 
-        source={require('../assets/images/bg.jpg')} 
-        style={{ width: '100%', height: '100%' }}
-        className="absolute w-full h-full" />
+      <View className="flex-1 relative">
+        <StatusBar style="light" />
+        <Image
+          blurRadius={5}
+          source={require('../assets/images/bg.jpg')}
+          style={{ width: '100%', height: '100%' }}
+          className="absolute w-full h-full" />
         {
-          loading? (
+          loading ? (
             <View className="flex-1 flex-row justify-center items-center">
               <Progress.CircleSnail thickness={10} size={140} color="#0bb3b2" />
             </View>
-          ):(
+          ) : (
             <SafeAreaView className="flex flex-1">
               {/* search section */}
-              <View style={{height: '7%'}} className="mx-4 relative z-50">
-                <View 
-                  className="flex-row justify-end items-center rounded-full" 
-                  style={{backgroundColor: showSearch? theme.bgWhite(0.2): 'transparent'}}>
-                  
+              <View style={{ height: '7%' }} className="mx-4 relative z-50">
+                <View
+                  className="flex-row justify-end items-center rounded-full"
+                  style={{ backgroundColor: showSearch ? theme.bgWhite(0.2) : 'transparent' }}>
+
+                  {
+                    showSearch ? (
+                      <TextInput
+                        onChangeText={handleTextDebounce}
+                        placeholder="Search city"
+                        placeholderTextColor={'lightgray'}
+                        className="pl-6 h-10 pb-1 flex-1 text-base text-white"
+                      />
+                    ) : null
+                  }
+                  <TouchableOpacity
+                    onPress={() => toggleSearch(!showSearch)}
+                    className="rounded-full p-3 m-1"
+                    style={{ backgroundColor: theme.bgWhite(0.3) }}>
                     {
-                      showSearch? (
-                        <TextInput 
-                          onChangeText={handleTextDebounce} 
-                          placeholder="Search city" 
-                          placeholderTextColor={'lightgray'} 
-                          className="pl-6 h-10 pb-1 flex-1 text-base text-white" 
-                        />
-                      ):null
+                      showSearch ? (
+                        <XMarkIcon size="25" color="white" />
+                      ) : (
+                        <MagnifyingGlassIcon size="25" color="white" />
+                      )
                     }
-                    <TouchableOpacity 
-                      onPress={()=> toggleSearch(!showSearch)} 
-                      className="rounded-full p-3 m-1" 
-                      style={{backgroundColor: theme.bgWhite(0.3)}}>
-                      {
-                        showSearch? (
-                          <XMarkIcon size="25" color="white" />
-                        ):(
-                          <MagnifyingGlassIcon size="25" color="white" />
-                        )
-                      }
-                      
+
                   </TouchableOpacity>
                 </View>
                 {
-                  locations.length>0 && showSearch?(
+                  locations.length > 0 && showSearch ? (
                     <View className="absolute w-full bg-gray-300 top-16 rounded-3xl ">
                       {
-                        locations.map((loc, index)=>{
-                          let showBorder = index+1 != locations.length;
-                          let borderClass = showBorder? ' border-b-2 border-b-gray-400':'';
+                        locations.map((loc, index) => {
+                          let showBorder = index + 1 != locations.length;
+                          let borderClass = showBorder ? ' border-b-2 border-b-gray-400' : '';
                           return (
-                            <TouchableOpacity 
+                            <TouchableOpacity
                               key={index}
-                              onPress={()=> handleLocation(loc)} 
-                              className={"flex-row items-center border-0 p-3 px-4 mb-1 "+borderClass}>
-                                <MapPinIcon size="20" color="gray" />
-                                <Text className="text-black text-lg ml-2">{loc?.name}, {loc?.country}</Text>
+                              onPress={() => handleLocation(loc)}
+                              className={"flex-row items-center border-0 p-3 px-4 mb-1 " + borderClass}>
+                              <MapPinIcon size="20" color="gray" />
+                              <Text className="text-black text-lg ml-2">{loc?.name}, {loc?.country}</Text>
                             </TouchableOpacity>
                           )
                         })
                       }
                     </View>
-                  ):null
+                  ) : null
                 }
-                
+
               </View>
 
               {/* forecast section */}
               <View className="mx-4 flex justify-around flex-1 mb-2">
                 {/* location */}
                 <Text className="text-white text-center text-2xl font-bold">
-                  {location?.name}, 
+                  {location?.name},
                   <Text className="text-lg font-semibold text-gray-300">{location?.country}</Text>
                 </Text>
                 {/* weather icon */}
                 <View className="flex-row justify-center">
-                  <Image 
+                  <Image
                     // source={{uri: 'https:'+current?.condition?.icon}} 
-                    source={weatherImages[current?.condition?.text || 'other']} 
+                    source={weatherImages[current?.condition?.text || 'other']}
                     className="w-52 h-52" />
-                  
+
                 </View>
                 {/* degree celcius */}
                 <View className="space-y-2">
-                    <Text className="text-center font-bold text-white text-6xl ml-5">
-                      {current?.temp_c}&#176;
-                    </Text>
-                    <Text className="text-center text-white text-xl tracking-widest">
-                      {current?.condition?.text}
-                    </Text>
+                  <Text className="text-center font-bold text-white text-6xl ml-5">
+                    {current?.temp_c}&#176;
+                  </Text>
+                  <Text className="text-center text-white text-xl tracking-widest">
+                    {current?.condition?.text}
+                  </Text>
                 </View>
 
                 {/* other stats */}
@@ -172,10 +174,10 @@ export default function HomeScreen() {
                   <View className="flex-row space-x-2 items-center">
                     <Image source={require('../assets/icons/sun.png')} className="w-6 h-6" />
                     <Text className="text-white font-semibold text-base">
-                      { weather?.forecast?.forecastday[0]?.astro?.sunrise }
+                      {weather?.forecast?.forecastday[0]?.astro?.sunrise}
                     </Text>
                   </View>
-                  
+
                 </View>
               </View>
 
@@ -183,30 +185,30 @@ export default function HomeScreen() {
               <View className="mb-2 space-y-3">
                 <View className="flex-row items-center mx-5 space-x-2">
                   <CalendarDaysIcon size="22" color="white" />
-                  <Text className="text-white text-base">Daily forecast</Text>
+                  <Text className="text-white text-base">Daily Forecast</Text>
                 </View>
-                <ScrollView   
+                <ScrollView
                   horizontal
-                  contentContainerStyle={{paddingHorizontal: 15}}
+                  contentContainerStyle={{ paddingHorizontal: 15 }}
                   showsHorizontalScrollIndicator={false}
                 >
                   {
-                    weather?.forecast?.forecastday?.map((item,index)=>{
+                    weather?.forecast?.forecastday?.map((item, index) => {
                       const date = new Date(item.date);
                       const options = { weekday: 'long' };
                       let dayName = date.toLocaleDateString('en-US', options);
                       dayName = dayName.split(',')[0];
 
                       return (
-                        <View 
-                          key={index} 
-                          className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4" 
-                          style={{backgroundColor: theme.bgWhite(0.15)}}
+                        <View
+                          key={index}
+                          className="flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4"
+                          style={{ backgroundColor: theme.bgWhite(0.15) }}
                         >
-                          <Image 
+                          <Image
                             // source={{uri: 'https:'+item?.day?.condition?.icon}}
                             source={weatherImages[item?.day?.condition?.text || 'other']}
-                              className="w-11 h-11" />
+                            className="w-11 h-11" />
                           <Text className="text-white">{dayName}</Text>
                           <Text className="text-white text-xl font-semibold">
                             {item?.day?.avgtemp_c}&#176;
@@ -215,15 +217,50 @@ export default function HomeScreen() {
                       )
                     })
                   }
-                  
+
                 </ScrollView>
               </View>
 
-              
-              <View className = "flex-row items-center mx-5 space-x-2">
-                <Image source={require('../assets/icons/sun.png')} className="w-60 h-60"></Image>
+              <View style={{ marginTop: 20, paddingHorizontal: 10, alignItems : 'center'}}>
+                <Text style={{ fontSize: 16, color: 'white' }} className = 'text-base'>Daily Forecast (Temperature)</Text>
+                <LineChart
+                  data={{
+                    labels: weather?.forecast?.forecastday?.map((item, index) => index.toString()),
+                    datasets: [
+                      {
+                        data: weather?.forecast?.forecastday?.map((item) => item?.day?.avgtemp_c || 0),
+                      },
+                    ],
+                  }}
+                  // alignment = "center"
+                  width={350} // from react-native
+                  height={200}
+                  xAxisLabel=' day'
+                  yAxisLabel="°C "
+                  withVerticalLabels={true}
+                  chartConfig={{
+                    backgroundColor: theme.bgWhite(0.2), // Background color of the chart
+                    // backgroundGradientFrom: "#EBE4D1", // Background gradient (from)
+                    // backgroundGradientTo: "#EBE4D1", // Background gradient (to)
+                    decimalPlaces: 1, // Number of decimal places for labels
+                    color: (opacity = 0) => "white", // Color of the data lines
+                    labelColor: (opacity = 0) => "white", // Color of the axis labels
+                    propsForDots: {
+                      r: '4', // Radius of the dots on the chart
+                      strokeWidth: '2',
+                      stroke: '#0bb3b2',
+                    },
+                  }}
+                  withVerticalLines={false}
+                  withHorizontalLines={false}
+                  bezier
+                  style={{
+                    marginVertical: 8,
+                    borderRadius: 16,
+                  }}
+                />
               </View>
-            
+
             </SafeAreaView>
           )
         }
